ec1a782a16ddb9429b1a53abb7daa8dc79a5d269
Michal Bochnak <mbochn2@uic.edu>
2018-09-09 17:48:42 -0500

    Final version

---
+++ ChicagoCrime/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ChicagoCrime
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.cmdByYear = new System.Windows.Forms.Button();
+      this.txtFilename = new System.Windows.Forms.TextBox();
+      this.cmdArrested = new System.Windows.Forms.Button();
+      this.cmdOneArea = new System.Windows.Forms.Button();
+      this.cmdChicagoAreas = new System.Windows.Forms.Button();
+      this.txtArea = new System.Windows.Forms.TextBox();
+      this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // cmdByYear
+      // 
+      this.cmdByYear.Location = new System.Drawing.Point(12, 28);
+      this.cmdByYear.Name = "cmdByYear";
+      this.cmdByYear.Size = new System.Drawing.Size(94, 82);
+      this.cmdByYear.TabIndex = 0;
+      this.cmdByYear.Text = "By Year";
+      this.cmdByYear.UseVisualStyleBackColor = true;
+      this.cmdByYear.Click += new System.EventHandler(this.cmdByYear_Click);
+      // 
+      // txtFilename
+      // 
+      this.txtFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.txtFilename.BackColor = System.Drawing.SystemColors.Control;
+      this.txtFilename.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.txtFilename.Location = new System.Drawing.Point(122, 477);
+      this.txtFilename.Name = "txtFilename";
+      this.txtFilename.Size = new System.Drawing.Size(769, 30);
+      this.txtFilename.TabIndex = 2;
+      this.txtFilename.Text = "CrimeDB.mdf";
+      // 
+      // cmdArrested
+      // 
+      this.cmdArrested.Location = new System.Drawing.Point(12, 134);
+      this.cmdArrested.Name = "cmdArrested";
+      this.cmdArrested.Size = new System.Drawing.Size(94, 61);
+      this.cmdArrested.TabIndex = 3;
+      this.cmdArrested.Text = "Arrested";
+      this.cmdArrested.UseVisualStyleBackColor = true;
+      this.cmdArrested.Click += new System.EventHandler(this.cmdArrested_Click);
+      // 
+      // cmdOneArea
+      // 
+      this.cmdOneArea.Location = new System.Drawing.Point(12, 236);
+      this.cmdOneArea.Name = "cmdOneArea";
+      this.cmdOneArea.Size = new System.Drawing.Size(94, 61);
+      this.cmdOneArea.TabIndex = 6;
+      this.cmdOneArea.Text = "One Area";
+      this.cmdOneArea.UseVisualStyleBackColor = true;
+      this.cmdOneArea.Click += new System.EventHandler(this.cmdOneArea_Click);
+      // 
+      // cmdChicagoAreas
+      // 
+      this.cmdChicagoAreas.Location = new System.Drawing.Point(12, 378);
+      this.cmdChicagoAreas.Name = "cmdChicagoAreas";
+      this.cmdChicagoAreas.Size = new System.Drawing.Size(94, 61);
+      this.cmdChicagoAreas.TabIndex = 7;
+      this.cmdChicagoAreas.Text = "Chicago Areas";
+      this.cmdChicagoAreas.UseVisualStyleBackColor = true;
+      this.cmdChicagoAreas.Click += new System.EventHandler(this.cmdChicagoAreas_Click);
+      // 
+      // txtArea
+      // 
+      this.txtArea.Location = new System.Drawing.Point(12, 304);
+      this.txtArea.Name = "txtArea";
+      this.txtArea.Size = new System.Drawing.Size(95, 34);
+      this.txtArea.TabIndex = 8;
+      this.txtArea.Text = "Loop";
+      this.txtArea.TextChanged += new System.EventHandler(this.txtArea_TextChanged);
+      // 
+      // chart
+      // 
+      this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      chartArea2.Name = "ChartArea1";
+      this.chart.ChartAreas.Add(chartArea2);
+      legend2.Name = "Legend1";
+      this.chart.Legends.Add(legend2);
+      this.chart.Location = new System.Drawing.Point(122, 12);
+      this.chart.Name = "chart";
+      series2.ChartArea = "ChartArea1";
+      series2.Legend = "Legend1";
+      series2.Name = "Series1";
+      this.chart.Series.Add(series2);
+      this.chart.Size = new System.Drawing.Size(769, 459);
+      this.chart.TabIndex = 9;
+      this.chart.Text = "chart1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(904, 511);
+      this.Controls.Add(this.chart);
+      this.Controls.Add(this.txtArea);
+      this.Controls.Add(this.cmdChicagoAreas);
+      this.Controls.Add(this.cmdOneArea);
+      this.Controls.Add(this.cmdArrested);
+      this.Controls.Add(this.txtFilename);
+      this.Controls.Add(this.cmdByYear);
+      this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.Margin = new System.Windows.Forms.Padding(6);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Chicago Crime Analysis";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button cmdByYear;
+    private System.Windows.Forms.TextBox txtFilename;
+    private System.Windows.Forms.Button cmdArrested;
+    private System.Windows.Forms.Button cmdOneArea;
+    private System.Windows.Forms.Button cmdChicagoAreas;
+    private System.Windows.Forms.TextBox txtArea;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+  }
+}
+
---
+++ ChicagoCrime/Form1.cs
@@ -0,0 +1,447 @@
+﻿//
+// GUI app to analyze Chicago crime data, using SQL and ADO.NET
+//
+// << Michal Bochnak, mbochn2 >>
+// U. of Illinois, Chicago
+// CS341, Fall2017
+// Project 06
+//
+
+using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ChicagoCrime
+{
+  public partial class Form1 : Form
+  {
+     public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      this.clearForm();
+    }
+
+    private bool fileExists(string filename)
+    {
+      if (!System.IO.File.Exists(filename))
+      {
+        string msg = string.Format("Input file not found: '{0}'",
+          filename);
+
+        MessageBox.Show(msg);
+        return false;
+      }
+
+      // exists!
+      return true;
+    }
+
+    private void clearForm()
+    {
+      this.chart.Series.Clear();
+      this.chart.Titles.Clear();
+      this.chart.Legends.Clear();
+    }
+
+    private void cmdByYear_Click(object sender, EventArgs e)
+    {
+      //
+      // Check to make sure database filename in text box actually exists:
+      //
+      string filename = this.txtFilename.Text;
+
+      if (!fileExists(filename))
+        return;
+
+      this.Cursor = Cursors.WaitCursor;
+
+      clearForm();
+
+      //
+      // Retrieve data from database:
+      //
+      string version = "MSSQLLocalDB";
+      string connectionInfo = String.Format(@"Data Source=(LocalDB)\{0};
+          AttachDbFilename=|DataDirectory|\{1};Integrated Security=True;",
+          version, filename);
+      SqlConnection db = new SqlConnection(connectionInfo);
+      db.Open();
+      
+      // create sql request
+      string sql = @"SELECT Year, Count(*) AS Total
+            FROM Crimes
+            GROUP BY Year
+            ORDER BY Year ASC";
+    
+      // process and fill dataset
+      SqlCommand cmd;
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+      DataSet ds = new DataSet();
+      cmd.CommandText = sql;
+      adapter.Fill(ds);
+
+      // close database
+      db.Close();   
+      
+      //
+      // Build a set of (x,y) points for plotting:
+      //
+      List<int> X = new List<int>();
+      List<int> Y = new List<int>();
+
+      foreach (DataRow row in ds.Tables["TABLE"].Rows)
+      {
+        X.Add(Convert.ToInt32(row["Year"]));
+        Y.Add(Convert.ToInt32(row["Total"]));
+      }
+
+      //
+      // now graph as a line chart:
+      //
+      this.chart.Titles.Add("Total # of Crimes Per Year");
+
+      var series = this.chart.Series.Add("total # of crimes");
+
+      series.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series.Points.AddXY(X[i], Y[i]);
+      }
+
+      var legend = new Legend();
+      legend.Docking = Docking.Top;
+      this.chart.Legends.Add(legend);
+
+      // 
+      // done:
+      //
+      this.Cursor = Cursors.Default;
+    }
+
+    private void cmdArrested_Click(object sender, EventArgs e)
+    {
+      //
+      // Check to make sure database filename in text box actually exists:
+      //
+      string filename = this.txtFilename.Text;
+
+      if (!fileExists(filename))
+        return;
+
+      this.Cursor = Cursors.WaitCursor;
+
+      clearForm();
+
+      //
+      // NOTE: you can do this with one SQL query by summing the
+      // Arrested column.  Alternatively, you can execute 2 queries,
+      // one to get the total counts, and then another to just 
+      // count where an arrest was made.
+      //
+
+      // retrieve database
+      string version = "MSSQLLocalDB";
+      string connectionInfo = String.Format(@"Data Source=(LocalDB)\{0};
+          AttachDbFilename=|DataDirectory|\{1};Integrated Security=True;",
+          version, filename);
+      SqlConnection db = new SqlConnection(connectionInfo);
+      db.Open();
+      
+      // prepare sql request
+      string sql = @"SELECT Year, Count(*) AS Total, Sum(CONVERT(float, Arrested)) AS Arrested
+            FROM Crimes
+            GROUP BY Year
+            ORDER BY Year ASC";
+
+      // process and fill dataset
+      SqlCommand cmd;
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+      DataSet ds = new DataSet();
+      cmd.CommandText = sql;
+      adapter.Fill(ds);
+
+      // close database
+      db.Close();
+
+      //
+      // Build a set of (x,y) points for plotting:
+      //
+      List<int> X = new List<int>();
+      List<int> Y1 = new List<int>();
+      List<int> Y2 = new List<int>();
+
+      foreach (DataRow row in ds.Tables["TABLE"].Rows)
+      {
+        X.Add(Convert.ToInt32(row["Year"]));
+        Y1.Add(Convert.ToInt32(row["Total"]));
+        Y2.Add(Convert.ToInt32(row["Arrested"]));
+      }
+
+      //
+      // now graph as a line chart:
+      //
+      this.chart.Titles.Add("Total # of Crimes Per Year vs. Number Arrested");
+
+      var series = this.chart.Series.Add("total # of crimes");
+
+      series.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series.Points.AddXY(X[i], Y1[i]);
+      }
+
+      var series2 = this.chart.Series.Add("# arrested");
+
+      series2.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series2.Points.AddXY(X[i], Y2[i]);
+      }
+
+      var legend = new Legend();
+      legend.Docking = Docking.Top;
+      this.chart.Legends.Add(legend);
+
+      //
+      // done:
+      //
+      this.Cursor = Cursors.Default;
+    }
+
+    private void cmdOneArea_Click(object sender, EventArgs e)
+    {
+      //
+      // Check to make sure database filename in text box actually exists:
+      //
+      string filename = this.txtFilename.Text;
+
+      if (!fileExists(filename))
+        return;
+
+      this.Cursor = Cursors.WaitCursor;
+
+      clearForm();
+
+      //
+      // NOTE: you might be able to do this with one SQL query,
+      // but probably easier to just execute 2 queries: one to
+      // get the total counts, and then another to get the counts
+      // for the area specified by the user.  You may assume the
+      // area name entered by the user exists (though FYI using a 
+      // different type of join yields the necessary counts of 0
+      // for plotting, and then it always works no matter what the
+      // user enters).
+      //
+
+      // retrieve database
+      string version = "MSSQLLocalDB";
+      string connectionInfo = String.Format(@"Data Source=(LocalDB)\{0};
+          AttachDbFilename=|DataDirectory|\{1};Integrated Security=True;",
+          version, filename);
+      SqlConnection db = new SqlConnection(connectionInfo);
+      db.Open();
+
+      // prepare sql message to get total crimes by year
+      string sql = @"SELECT Year, Count(*) AS Total
+            FROM Crimes
+            GROUP BY Year
+            ORDER BY Year ASC";
+      
+      // process and fil dataset
+      SqlCommand cmd;
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+      DataSet ds = new DataSet();
+      cmd.CommandText = sql;
+      adapter.Fill(ds);
+
+      // prepare sql message to get the area code
+      sql = string.Format(@"SELECT Area FROM Areas WHERE AreaName = '{0}';", this.txtArea.Text);
+
+      // process request and mine the code
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      cmd.CommandText = sql;
+      object result = cmd.ExecuteScalar();
+      string area;
+      if (result == null)
+      {
+        area = "???";
+      }
+      else if (result == DBNull.Value)
+      {
+        area = "???";
+      }
+      else
+      {
+        area = Convert.ToString(result);
+      }
+
+      // prepare message to get crimes by area
+      sql = string.Format(@"SELECT Year, Count(*) AS Total
+            FROM Crimes
+            WHERE Area = {0}
+            GROUP BY Year
+            ORDER BY Year ASC;", area);
+
+      // process and fill dataset
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      adapter = new SqlDataAdapter(cmd);
+      DataSet ds2 = new DataSet();
+      cmd.CommandText = sql;
+      adapter.Fill(ds2);
+
+      // close database
+      db.Close();
+
+      //
+      // Build a set of (x,y) points for plotting:
+      //
+      List<int> X = new List<int>();
+      List<int> Y1 = new List<int>();
+      List<int> Y2 = new List<int>();
+
+      foreach (DataRow row in ds.Tables["TABLE"].Rows)
+      {
+        X.Add(Convert.ToInt32(row["Year"]));
+        Y1.Add(Convert.ToInt32(row["Total"]));
+      }
+
+      foreach (DataRow row in ds2.Tables["TABLE"].Rows)
+      {
+        Y2.Add(Convert.ToInt32(row["Total"]));
+      }
+
+      //
+      // now graph as a line chart:
+      //
+      this.chart.Titles.Add("Total # of Crimes Per Year vs. Particular Area");
+
+      var series = this.chart.Series.Add("total # of crimes");
+
+      series.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series.Points.AddXY(X[i], Y1[i]);
+      }
+
+      var series2 = this.chart.Series.Add("# in this area");
+
+      series2.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series2.Points.AddXY(X[i], Y2[i]);
+      }
+
+      var legend = new Legend();
+      legend.Docking = Docking.Top;
+      this.chart.Legends.Add(legend);
+
+      //
+      // done:
+      //
+      this.Cursor = Cursors.Default;
+    }
+
+    private void cmdChicagoAreas_Click(object sender, EventArgs e)
+    {
+      //
+      // Check to make sure database filename in text box actually exists:
+      //
+      string filename = this.txtFilename.Text;
+
+      if (!fileExists(filename))
+        return;
+
+      this.Cursor = Cursors.WaitCursor;
+
+      clearForm();
+
+      // retrieve database
+      string version = "MSSQLLocalDB";
+      string connectionInfo = String.Format(@"Data Source=(LocalDB)\{0};
+          AttachDbFilename=|DataDirectory|\{1};Integrated Security=True;",
+          version, filename);
+      SqlConnection db = new SqlConnection(connectionInfo);
+      db.Open();
+
+      // prepare sql message
+      string sql = @"SELECT Area, Count(*) AS Total
+            FROM Crimes
+            WHERE Area > 0
+            GROUP BY Area
+            ORDER BY Area ASC";
+      
+      // process request and fill dataset
+      SqlCommand cmd;
+      cmd = new SqlCommand();
+      cmd.Connection = db;
+      SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+      DataSet ds = new DataSet();
+      cmd.CommandText = sql;
+      adapter.Fill(ds);
+
+      // close database
+      db.Close();
+
+      //
+      // Build a set of (x,y) points for plotting:
+      //
+      List<int> X = new List<int>();
+      List<int> Y = new List<int>();
+
+      foreach (DataRow row in ds.Tables["TABLE"].Rows)
+      {
+        X.Add(Convert.ToInt32(row["Area"]));
+        Y.Add(Convert.ToInt32(row["Total"]));
+      }
+
+      //
+      // now graph as a line chart:
+      //
+      this.chart.Titles.Add("Total # of Crimes in each Chicago Area");
+
+      var series = this.chart.Series.Add("total # of crimes");
+
+      series.ChartType = SeriesChartType.Line;
+
+      for (int i = 0; i < X.Count; ++i)
+      {
+        series.Points.AddXY(X[i], Y[i]);
+      }
+
+      var legend = new Legend();
+      legend.Docking = Docking.Top;
+      this.chart.Legends.Add(legend);
+
+      //
+      // done:
+      //
+      this.Cursor = Cursors.Default;
+    }
+
+    private void txtArea_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }//class
+}//namespace
